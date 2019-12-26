a74f4447b146829ce695ab0128ded00f20b54fa9
Hanifa Arrumaisha <hanifa.aisha@gmail.com>
2019-12-27 03:43:09 +0700

    add api call function for prayer time

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/hanifa/odoj/utils/APICall.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/hanifa/odoj/utils/APICall.java
@@ -1,4 +1,69 @@
 package id.ac.ui.cs.mobileprogramming.hanifa.odoj.utils;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import id.ac.ui.cs.mobileprogramming.hanifa.odoj.data.entity.PrayerTime;
+import id.ac.ui.cs.mobileprogramming.hanifa.odoj.data.entity.PrayerTimeDTO;
+import id.ac.ui.cs.mobileprogramming.hanifa.odoj.notification.PrayerNotification;
+
 public class APICall {
+    Context context;
+
+    public static final int SHUROOQ = 1;
+    public static final int DHUHR = 2;
+    public static final int ASR = 3;
+    public static final int MAGHRIB = 4;
+    public static final int ISHA = 5;
+
+    public APICall(Context context) {
+        this.context = context;
+    }
+
+    public void requestPrayerTimes(){
+        String url = "https://muslimsalat.com/jakarta/daily.json?key=283714fbf745c829c7163e0b9dfa0cbf";
+        RequestQueue rq = Volley.newRequestQueue(context);
+        rq.start();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url,(JSONObject) null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Gson gson = new Gson();
+                    PrayerTimeDTO[] prayerTimeDTO = gson.fromJson(response.getString("items"), PrayerTimeDTO[].class);
+                    PrayerTime prayerTime = new PrayerTime(prayerTimeDTO[0]);
+                    System.out.println("REQUEST API");
+                    System.out.println(prayerTime.getDhuhr());
+                    createNofication(prayerTime, context);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }},  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Can't retrieve API", Toast.LENGTH_LONG).show();
+            }});
+        rq.add(jsonObjectRequest);
+    }
+
+    private void createNofication(PrayerTime prayerTime, Context context) {
+        PrayerNotification notifShurooq = new PrayerNotification(context, prayerTime.getShurooq(), SHUROOQ);
+        PrayerNotification notifDhuhr = new PrayerNotification(context, prayerTime.getDhuhr(), DHUHR);
+        PrayerNotification notifAsr = new PrayerNotification(context, prayerTime.getAsr(), ASR);
+        System.out.println("CEK PRAYER TIME NOTIF");
+        System.out.println(prayerTime.getIsha().getTime());
+        PrayerNotification notifMaghrib = new PrayerNotification(context, prayerTime.getMaghrib(), MAGHRIB);
+        System.out.println(prayerTime.getIsha().getTime());
+        PrayerNotification notifIsha = new PrayerNotification(context, prayerTime.getIsha(), ISHA);
+    }
 }
